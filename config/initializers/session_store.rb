0288c3f9be5b1a0da87d2be240762d5ed2847585
Michiharu Ono <michiharuono77@gmail.com>
2022-01-15 13:04:44 +0900

    session_store.rb in config is set for the production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Here we define what the cookie passed from the front-end structured like
-
-Rails.application.config.session_store :cookie_store, key:"_authentication_app", domain: "https://HerokuAppNameHere.herokuapp.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key:"_authentication_app", domain: "https://HerokuAppNameHere.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key:"_authentication_app"
+end 
