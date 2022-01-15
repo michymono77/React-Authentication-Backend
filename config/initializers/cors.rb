2e28bb11af56104125f2563337220bce4f0daefa
Michiharu Ono <michiharuono77@gmail.com>
2022-01-15 11:16:09 +0900

    The credential file for cors.rb edited

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,16 @@
 # Cors gives you the ability to whitelist certain domains
+# credentials true will allow you to pass front-end cookie to backend
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origin "http://localhost:3000/"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
+  end
+
+  allow do
+    origin "https://HerokuAppNameHere.herokuapp.com"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
   end
 end
